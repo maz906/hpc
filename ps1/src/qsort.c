a94db7f65c8bb77353210444871db9c3ce421cf1
maz906 <mz84092@gmail.com>
2016-01-13 17:05:21 -0700

    makefile changes

--- ps1/src/qsort.c
+++ ps1/src/qsort.c
@@ -1,7 +1,26 @@
 #include "qsort.h"
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv) 
+{
 
 }
 
+/*
+ * base - pointer to the first object of the array to be sorted
+ * num - number of elements in the array pointed to by base
+ * size - size in bytes of each element of the array
+ * compar - pointer to function that compares two elements  
+ */
+void qsort(void* base, size_t num, size_t size,
+						int (*compar)(const void*, const void*))
+{
 
+	//singleton array, so we are done
+	if (num <= 1)
+		return;	
+	
+	//pivot
+	void* last = base + (num - 1);
+
+
+}
